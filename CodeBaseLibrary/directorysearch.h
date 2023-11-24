d4b98a8ef4b29c8bf8264d12ca4021bf2ea0d70a
RetroBytes32 <66331916+RetroBytes32@users.noreply.github.com>
2023-11-23 18:06:53 -0700

    Delete CodeBaseLibrary directory

--- CodeBaseLibrary/directorysearch.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DIRECTORY_SEARCH__
-#define _DIRECTORY_SEARCH__
-
-#include <vector>
-#include <string>
-
-__declspec(dllexport) std::vector<std::string> DirectoryGetList(std::string path);
-
-#endif
